557f1f3c0a73241e981ad1b82afcca3ff7f999f0
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-02-29 03:53:59 +0500

    added icons and stuff

---
+++ lib/classes_and_vars/temp_ui_classes.dart
@@ -0,0 +1,6 @@
+class Patient {
+  String name;
+  DateTime admittedOn;
+
+  Patient({required this.name, required this.admittedOn});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'dart:async';
-import 'dart:math';
+// Introduction:
+// This is a simple flutter application that is primarily designed for reading and writing data to a local database for persistent storage. The purpose is to take the data related to the patient and store it on a relational database using the SQflite package available as a package for flutter.
+// Here is an overview of what the interface is going to look like:
+// The name of the hospital is S.Sohail Hospital
+// Home screen overview:
+// At the home screen we will have a search in the app bar then the list view of the list tiles for the patients. We can add a patient using the floating action button. Here is what happens when we tap the floating action button. An alert dialogue box appears asking for the basic details about the patient. When the patient is created, it appears on the home screen as the listview updates.
+// On tapping the List tile of the patient, we should be navigated to the patient screen, where we are going to be able to see the patient’s information which is specified below:
+
+// The Patient Screen:
+// At the patient screen, we can set the appointment. The appbar should have the name of the patient, afterwards, we select the type of appointment whether it is an emergency or a visit using the radio buttons.
+// Then we will have a description of the diagnosis. Which we will get using a Tex field. We also use a field for the amount charged per visit. On the right side we have an update button to add the visit to the history.
+// Then we will have an expandable list tile to view the details of the patient.
+// After this we will have the history of the patient which will be an expandable list containing list tiles about the history of the visits.
+// Afterwards we have the bill section that will show the total bill for the patient and the number of visits. And on the right side we will have the Pay Bills button.
+// On tapping the pay Bills button an alert dialogue box shows up showing whether we want to pay bills using the insurance or direct method. The entered amount is deducted from the total bill.
 
 import 'package:flutter/material.dart';
+import 'package:s_sohail/classes_and_vars/temp_ui_classes.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -99,7 +113,18 @@ class HomeScreen extends StatefulWidget {
   final bool useLightMode;
   final VoidCallback handleBrightnessChange;
 
-  const HomeScreen({
+  List<Patient> patientList = [
+    Patient(
+      name: 'John Doe',
+      admittedOn: DateTime.now(),
+    ),
+    Patient(
+      name: 'Jane Doe',
+      admittedOn: DateTime.now(),
+    ),
+  ];
+
+  HomeScreen({
     Key? key,
     required this.useLightMode,
     required this.handleBrightnessChange,
@@ -118,26 +143,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //creating a simple drawer for theme setting and about me page
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Settings'),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            ListTile(
+              title: const Text('Light Mode'),
+              onTap: () {
+                widget.handleBrightnessChange();
+              },
+            ),
+            ListTile(
+              title: const Text('Dark Mode'),
+              onTap: () {
+                widget.handleBrightnessChange();
+              },
+            ),
+            ListTile(
+              title: const Text('Material 3'),
+              onTap: () {
+                // Add your onPressed code here!
+              },
+            ),
+            ListTile(
+              title: const Text('About Project'),
+              trailing: Icon(Icons.info_outline),
+              onTap: () {
+                // https://github.com/HaseebKahn365/s_sohail_hospital
+                launchUrl(Uri.parse('https://github.com/HaseebKahn365/s_sohail_hospital'));
+              },
+            ),
+          ],
+        ),
+      ),
+
       appBar: AppBar(
         title: const Text('S. Sohail Hospital'),
         //adding an action to toggle the theme
         actions: <Widget>[
           //adding an exclamation icon button that launches the about me page
-          IconButton(
-            icon: const Icon(Icons.info_outline),
-            onPressed: () {
-              // https://github.com/HaseebKahn365/s_sohail_hospital
-              launchUrl(Uri.parse('https://github.com/HaseebKahn365/s_sohail_hospital'));
-            },
-          ),
+
           IconButton(
             icon: Icon(widget.useLightMode ? Icons.dark_mode : Icons.light_mode),
             onPressed: widget.handleBrightnessChange,
           ),
         ],
       ),
-      body: Center(
-        child: Text("Hello World"),
+      //List view for widgets
+      body: ListView(
+        //make widget occupy min space
+        shrinkWrap: true,
+
+        children: [
+          //create an add patient button
+          Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Add your onPressed code here!
+                },
+                child: const Text('Add Patient'),
+              ),
+            ],
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
